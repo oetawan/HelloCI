a8e4696cf15e35f3116f13fab7493538b357f1a5
oetawan <oetawan@gmail.com>
2013-06-16 02:10:11 +0700

    Added test to build script

---
+++ src/test/HelloCI.Tests/Web/RouteTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Web.Routing;
+using HelloCI.Web;
+using MvcContrib.TestHelper;
+using HelloCI.Web.Controllers;
+using System.Web;
+using System.Web.Mvc;
+namespace HelloCI.Tests.Web
+{
+    [TestFixture]
+    public class RouteTests
+    {
+        [Test]
+        public void SomeTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
