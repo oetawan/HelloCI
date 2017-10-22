0ca809d182a1611a87703112820d2de1d51975b7
MITRAIS\oetawan_c <oetawanarya.chandra@mitrais.com>
2017-10-22 18:38:00 +0700

    build config

--- built.bat
+++ built.bat
@@ -1,1 +1,1 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild HelloCI.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild .\src\app\HelloCI.Web\HelloCI.Web.csproj /t:Rebuild /p:Configuration=Release
